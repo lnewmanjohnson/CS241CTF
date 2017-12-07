8b2f0f4dc1b454fe521c7c22aef3d6c36db160f7
Peter Connolly <pconnolly18@amherst.edu>
2017-12-07 13:11:24 -0500

    Update

--- myTeam.py
+++ myTeam.py
@@ -55,7 +55,7 @@ class TestAgent(CaptureAgent):
     """
 
     beliefs = util.Counter()  # Keep track of beliefs about location of opponents (tuple of 2 positions with prob)
-    agents = [0, 0]
+    agents = [0, 0, 0, 0]
 
     def registerInitialState(self, gameState):
         """
@@ -90,37 +90,51 @@ class TestAgent(CaptureAgent):
         TestAgent.beliefs[tuple(belief)] = 1.0
 
         self.start = gameState.getAgentPosition(self.index)  # Start position
+        self.safety_score = 100
 
     def chooseAction(self, gameState):
         """
         Picks among actions randomly.
         """
-        if self.index > 1:
-            index = 0
-        else:
-            index = 1
 
-        teammate_index = (self.index + 2) % 4
-        teammate = gameState.getAgentState(teammate_index)
+        #teammate_index = (self.index + 2) % 4
+        #teammate = gameState.getAgentState(teammate_index)
 
         # TODO: Update beliefs based on new observations and possible moves
-        beliefs = TestAgent.beliefs
-        # print(gameState.getAgentDistances())
-        # print(beliefs)
 
-        actions = gameState.getLegalActions(self.index)
-        if TestAgent.agents[index] == 0:
-            return self.search(gameState, actions)
+        time.sleep(0.2)
 
-    def search(self, gameState, actions):
+        if TestAgent.agents[self.index] == 0:
+            print("Agent %d: Searching" % self.index)
+            return self.search(gameState)
+        elif TestAgent.agents[self.index] == 1:
+            print("Agent %d: Delivering" % self.index)
+            return self.deliver_food(gameState)
+
+    def update_safety_score(self, gameState):
+        self.safety_score = self.get_safety_score(gameState)
+
+    def get_safety_score(self, gameState):
+        my_pos = gameState.getAgentPosition(self.index)
+        safety_score = 100
+        if(my_pos[0] > 16 and gameState.getAgentState(self.index).scaredTimer == 0):
+            return safety_score
+        else:
+            opps_index = [(self.index + 1) % 4, (self.index + 3) % 4]
+            for opp in opps_index:
+                opp_state = gameState.getAgentState(opp)
+                opp_timer = opp_state.scaredTimer
+                opp_pos = gameState.getAgentPosition(opp)
+                if opp_pos is not None and (opp_pos[0]-16) < (16 - my_pos[0]):
+                    safety_score -= 10 * self.getMazeDistance(my_pos, opp_pos)
+            return safety_score
+
+    def search(self, gameState):
         """
         Offense 1: Aggressively searches for food, avoiding ghosts
         """
         def evaluate(self, gameState, action):
             score = 0.0
-            danger = 0
-            dist = 9999
-
             successor = self.getSuccessor(gameState, action)
             newPos = successor.getAgentState(self.index).configuration.pos
             if (successor.getAgentState(self.index).numCarrying > gameState.getAgentState(self.index).numCarrying):
@@ -129,16 +143,28 @@ class TestAgent(CaptureAgent):
             for food in self.getFood(successor).asList():
                 score += 10.0 / self.getMazeDistance(newPos, food)
 
-            score -= len(self.getFood(successor).asList())
-            print score
+            for index in self.getOpponents(gameState):
+                ghost = successor.getAgentState(index)
+                opp_pos = successor.getAgentPosition(index)
+                if (ghost.scaredTimer > 6 and opp_pos is not None):
+                    print(ghost.scaredTimer)
+                    score += 100/self.getMazeDistance(newPos, opp_pos)
+
+            score -= len(self.getFood(successor).asList())/10
+            score -= self.get_safety_score(successor)
             return score
 
-        # TODO: Checks for switching routines
+        self.update_safety_score(gameState)
+        if(self.safety_score < 50):
+            TestAgent.agents[self.index] = 1
+            return self.deliver_food(gameState)
+
         carrying = gameState.getAgentState(self.index).numCarrying
-        if (carrying > 6):
-            return self.deliver_food(gameState, actions)
+        if (carrying > 6 or self.safety_score < 50):
+            return self.deliver_food(gameState)
         # elif(other_checks): return other)subroutine2(gameState)
         else:
+            actions = gameState.getLegalActions(self.index)
             best_action = actions[0]
             best_score = -9999
 
@@ -150,33 +176,38 @@ class TestAgent(CaptureAgent):
 
         return best_action
 
-    def deliver_food(self, gameState, actions):
+    def deliver_food(self, gameState):
         """
         Offense 2: Return food to safety
         """
-        def evaluate(self, gameState, action):
-            #TODO: Keep other ghosts into account
-            score = 0.0
-            danger = 0
-            dist = 9999
 
+        def evaluate(self, gameState, action):
+            score = 0.0
             successor = self.getSuccessor(gameState, action)
             newPos = successor.getAgentState(self.index).configuration.pos
-
+            if(newPos[0] > 16):
+                return 10
             safety_spots = [(17, i) for i in [1, 3, 4, 6, 7, 8, 10, 14]]
             for pos in safety_spots:
                 score += 10.0 / (self.getMazeDistance(newPos, pos) ** 2)
+            score -= self.get_safety_score(successor)/10
             return score
 
-        best_action = actions[0]
-        best_score = -9999
-        for action in actions:
-            score = evaluate(self, gameState, action)
-            if (score > best_score):
-                best_score = score
-                best_action = action
-
-        return best_action
+        self.update_safety_score(gameState)
+        carrying = gameState.getAgentState(self.index).numCarrying
+        if (carrying < 6 and self.safety_score > 60):
+            TestAgent.agents[self.index] = 0
+            return self.search(gameState)
+        else:
+            actions = gameState.getLegalActions(self.index)
+            best_action = actions[0]
+            best_score = -9999
+            for action in actions:
+                score = evaluate(self, gameState, action)
+                if (score > best_score):
+                    best_score = score
+                    best_action = action
+            return best_action
 
     def getSuccessor(self, gameState, action):
         """
